769c4e3c238910f4838fe0ca117d4ef696ae9c1f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-02-03 03:42:16 +0100

    next stage of category analysis

--- categories/clean_categories.py
+++ categories/clean_categories.py
@@ -7,7 +7,7 @@ df = pd.read_csv('./categories.csv', header=0)
 df.drop_duplicates(inplace=True)
 
 # pozbywam się 'kategorii', które nie mają ofert (jak się okazuje są to linki do konfiguratorów)
-df.drop(df[df.offers_count == 'None'].index, inplace=True)
+df.drop(df[df.offers_count.isnull()].index, inplace=True)
 
 # konwertuje kolumnę offers_count do typu liczbowego
 df.offers_count = pd.to_numeric(df.offers_count)
