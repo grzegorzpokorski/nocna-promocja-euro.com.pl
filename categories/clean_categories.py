51a4d4b5909654d6e2dc74de0dc230e78ee3695b
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-02-03 15:14:23 +0100

    dodatkowe wykresy i refleksja odnośnie powtarzających się kategorii

--- categories/clean_categories.py
+++ categories/clean_categories.py
@@ -6,9 +6,12 @@ df = pd.read_csv('./categories.csv', header=0)
 # usuwam powtórzenia
 df.drop_duplicates(inplace=True)
 
+# usówam kategorie powtarzające się w kategoriach głównych
+df.drop_duplicates(subset=['link_to_category', 'offers_count'] ,inplace=True)
+
 # pozbywam się 'kategorii', które nie mają ofert (jak się okazuje są to linki do konfiguratorów)
 df.drop(df[df.offers_count.isnull()].index, inplace=True)
 
 # konwertuje kolumnę offers_count do typu liczbowego
 df.offers_count = pd.to_numeric(df.offers_count)
-df.to_csv('./categories.csv', index=False)
+df.to_csv('./categories_C.csv', index=False)
